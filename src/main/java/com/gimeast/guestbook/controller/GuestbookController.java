6185efc3f8e1e47381a6a395966b732f20926870
dudu <dudu.gimeast@gmail.com>
2023-06-07 21:25:00 +0900

    글 등록 개발완료

--- src/main/java/com/gimeast/guestbook/controller/GuestbookController.java
+++ src/main/java/com/gimeast/guestbook/controller/GuestbookController.java
@@ -9,7 +9,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -22,14 +24,35 @@ public class GuestbookController {
         this.guestbookService = guestbookService;
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
     @GetMapping("/list")
-    public String list(PageRequestDto pageRequestDto, Model model) {
+    public void list(PageRequestDto pageRequestDto, Model model) {
 
         log.info("list............." + pageRequestDto);
         PageResultDto<GuestbookDto, Guestbook> result = guestbookService.getList(pageRequestDto);
 
         model.addAttribute("result", result);
 
-        return"/guestbook/list";
     }
+
+    @GetMapping("/register")
+    public void register() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDto guestbookDto, RedirectAttributes redirectAttributes) {
+        log.info("guestbookDto........", guestbookDto);
+        Long gno = guestbookService.register(guestbookDto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
+    }
+
+
+
 }
